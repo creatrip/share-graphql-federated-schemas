4c00f75c5e5e47822666c83dae85b2fa056e7117
rhea-so <jeonghyeon.rhea@gmail.com>
2023-02-03 16:17:56 +0900

    [none] Local에서 동작하는 Sample 코드 추가

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello, World!');
+import * as fs from 'fs';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { loadSchema } from '@graphql-tools/load';
+import { mergeSchemas } from '@graphql-tools/schema';
+import { printSubgraphSchema } from '@apollo/subgraph';
+
+async function main() {
+  const base = await loadSchema('base.graphql', {
+    loaders: [new GraphQLFileLoader()],
+  });
+
+  const changed = await loadSchema('changed.graphql', {
+    loaders: [new GraphQLFileLoader()],
+  });
+
+  const gatewaySchema = mergeSchemas({ schemas: [base, changed] });
+  fs.writeFileSync('schema.graphql', printSubgraphSchema(gatewaySchema));
+}
+
+main();
